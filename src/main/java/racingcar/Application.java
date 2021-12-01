c505e2c501e054b4ae1c61a00a54a16bf7a79be3
wooseok <diddmlvkf@gmail.com>
2021-12-01 23:28:03 +0900

    refactor(Application) : depth를 2로 만들기 위해 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -10,35 +10,16 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO 구현 진행
 		// 경주할 자동차 이름 입력 및 게임 참가
-		Game game;
-		while (true) {
-			try {
-				game = new Game();
-				View.inputCarName();
-				String[] nameList = Console.readLine().split(",");
-				for (String carName : nameList) {
-					game.participate(new Car(carName));
-				}
-				break;
-			} catch (IllegalArgumentException e) {
-				System.out.println(e.getMessage());
-			}
+		Game game = new Game();
+		List<Car> cars = getCarList();
+		for (Car car : cars) {
+			game.participate(car);
 		}
 		// 시도할 회수 입력
-		String playCount;
-		while (true) {
-			try {
-				View.inputCount();
-				playCount = Console.readLine();
-				Validate.isNumber(playCount);
-				break;
-			} catch (IllegalArgumentException e) {
-				System.out.println(e.getMessage());
-			}
-		}
+		int playCount = getPlayCount();
 		// 실행 결과 현황판
 		View.result();
-		for (int play = 0; play < Integer.parseInt(playCount); play++) {
+		for (int play = 0; play < playCount; play++) {
 			game.play();
 			View.resultBoard(game.getCars());
 		}
@@ -46,8 +27,30 @@ public class Application {
 		View.winner(game.winners());
 	}
 
-	public static void participateGame(String nameList) {
-		List<Car> cars = new ArrayList<>();
+	public static List<Car> getCarList() {
+		try {
+			List<Car> cars = new ArrayList<>();
+			View.inputCarName();
+			String[] nameList = Console.readLine().split(",");
+			for (String carName : nameList) {
+				cars.add(new Car(carName));
+			}
+			return cars;
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getCarList();
+		}
+	}
 
+	public static int getPlayCount() {
+		try {
+			View.inputCount();
+			String input = Console.readLine();
+			Validate.isNumber(input);
+			return Integer.parseInt(input);
+		} catch (IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+			return getPlayCount();
+		}
 	}
 }

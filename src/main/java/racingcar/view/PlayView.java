680db8f301d044df6f40d30b4bb671c396f24353
wooseok <diddmlvkf@gmail.com>
2021-12-03 20:35:18 +0900

    feat(PlayView) : 실행 결과(현황판) UI 구현

---
+++ src/main/java/racingcar/view/PlayView.java
@@ -0,0 +1,27 @@
+package racingcar.view;
+
+import java.util.List;
+
+import racingcar.model.Car;
+
+public class PlayView {
+
+	private static final String PLAY_UI_MESSAGE = "실행 결과";
+
+	public static void playUI() {
+		System.out.println(PLAY_UI_MESSAGE);
+	}
+
+	public static void statusBoardUI(List<Car> cars) {
+		for (Car car : cars) {
+			System.out.printf("%s : %d\n",
+				car.getName(),
+				formatPosition(car.getPosition())
+			);
+		}
+	}
+
+	private static String formatPosition(int position) {
+		return new String(new char[position]).replace("\0", "-");
+	}
+}

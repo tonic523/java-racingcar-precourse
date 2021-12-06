cc4f94933cc2806908078d4af80e4924ef37615b
wooseok <diddmlvkf@gmail.com>
2021-12-06 21:33:31 +0900

    refactor(InputController) : 각 메서드에 UI 출력 기능 추가

--- src/main/java/racingcar/controller/InputController.java
+++ src/main/java/racingcar/controller/InputController.java
@@ -7,12 +7,15 @@ import racingcar.model.Car;
 import racingcar.utils.Convertation;
 import racingcar.utils.Validate;
 
+import static racingcar.view.InputView.*;
+
 public class InputController {
 
 	private static final String ERROR_MESSAGE = "[ERROR]";
 
 	public static List<Car> inputCarName() {
 		try {
+			carNameUI();
 			String input = Console.readLine();
 			Validate.containComma(input);
 			return Convertation.namesToCars(input.split(","));
@@ -24,6 +27,7 @@ public class InputController {
 
 	public static int inputCount() {
 		try {
+			countUI();
 			String input = Console.readLine();
 			Validate.isNumber(input);
 			Validate.isZero(input);

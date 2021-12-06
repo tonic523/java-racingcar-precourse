4cd339aeb73980b996a635b646dbcc5e014098f7
wooseok <diddmlvkf@gmail.com>
2021-12-06 20:57:29 +0900

    feat(Game) : 자동차들의 전진 조건, 랜덤값으로 게임 실행 구현

--- src/main/java/racingcar/model/Game.java
+++ src/main/java/racingcar/model/Game.java
@@ -1,13 +1,27 @@
 package racingcar.model;
 
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Game {
 	private List<Car> cars;
+	private Supplier<Integer> moveValue = () -> Randoms.pickNumberInRange(1, 9);
+	private Predicate<Integer> moveCondition = i -> i >= 4;
 
 	public Game(List<Car> cars) throws IllegalArgumentException {
 		validateLeastTwoCars(cars);
 		this.cars = cars;
+	}
+
+	public void play() {
+		for (Car car : cars) {
+			if (moveCondition.test(moveValue.get())) {
+				car.move();
+			}
+		}
 	}
 
 	private static void validateLeastTwoCars(List<Car> cars) throws IllegalArgumentException {
